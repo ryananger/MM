5979c5efe71ffc6528673004131b640614832bd1
Ryan Anger <anger.ryan.s@gmail.com>
2015-09-12 16:24:28 -0400

    Chat ' / ' functionality
    Nick update functional, and finished input shift states.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ io.on('connection', function (socket)
  		if (username == 'User') 
  		{
  			var newname = ('User ' + totalSocketCount);
- 			io.emit('updateName', sessionid, newname);
+ 			socket.emit('updateName', sessionid, newname);
  		};
 
  		var info = {id: sessionid, username: newname};
@@ -34,9 +34,43 @@ io.on('connection', function (socket)
   		io.emit('newUser', Sockets, info);
   	});
 
-  	socket.on('chatUpdate', function (entry) {
-  		io.emit('chatUpdate', entry);
-  	})
+  	socket.on('chatUpdate', function (id, entry) {
+
+  		var isFunction = function(entry) {
+  			return entry.text.slice(0,1) == '/'; 
+  		};
+
+  		if (isFunction(entry)) {
+  			var whatFunction = entry.text.slice(1,5);
+
+  			switch (whatFunction) {
+  				case 'nick':
+  					nameChange(id, entry);
+  					break;
+  			}
+  		}
+  		else {
+  			io.emit('chatUpdate', entry);
+  		}
+  		
+  	});
+
+  	var nameChange = function (id, entry) {
+  		var newname = entry.text.slice(6);
+
+  		for (var i in Sockets) {
+  			if (Sockets[i].id == id) {
+  				var oldname = Sockets[i].username;
+
+  				Sockets[i].username = newname;
+
+  				var systemEntry = {name: 'system', text: oldname + ' changed their username to ' + newname + '.'}
+
+  				io.emit('chatUpdate', systemEntry);
+  			}
+  		}
+  		io.emit('nameChange', id, newname);
+  	};
 
   	socket.on('disconnect', function () 
   	{
